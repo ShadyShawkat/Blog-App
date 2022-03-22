bfca1b09388beaab48269c3686e7f5355c765587
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-22 20:16:02 +0200

    Add show action for posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show] do
-    resources 'posts', on: :show, only: [:index]
+    resources 'posts', on: :show, only: [:index, :show]
   end
   # Defines the root path route ("/")
 

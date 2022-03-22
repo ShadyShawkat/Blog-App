70c33511a3360c377e1bcd6a80ba0243c4e140ea
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-22 19:45:51 +0200

    Add show action for users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
   # Defines the root path route ("/")
   # root "articles#index"
 end

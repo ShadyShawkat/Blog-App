1f528e3e2f40c805008f45585fe15afb33f82be9
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-28 20:18:25 +0200

    Add set_user_posts_counter method in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   belongs_to :user
+
+  def set_user_posts_counter
+    user.update(posts_counter: user.posts.count)
+  end
 end

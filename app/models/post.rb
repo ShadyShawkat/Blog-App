324f1bda8ef96f115300d777c2bff0067798b43e
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-31 22:23:59 +0200

    Validate post title

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
+  validates :title, presence: true, length: { maximum: 250 }
+
   after_save :set_user_posts_counter
   def set_user_posts_counter
     author.update(posts_counter: author.posts.count)

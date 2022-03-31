ed2c0deef6f48d788e32b7f522640b4bcf840b8b
ShadyShawkat <shadyshawkat91@gmail.com>
2022-04-01 00:18:19 +0200

    Add test for name presence in user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)}
 
+  before { subject.save }
+
+  it 'name should be present' do 
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 end

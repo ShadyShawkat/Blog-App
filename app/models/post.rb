591b5d2c01f44021e5d3637d841dc7a719d8fac8
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-31 22:24:32 +0200

    Validate post likes_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
   validates :title, presence: true, length: { maximum: 250 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :set_user_posts_counter
   def set_user_posts_counter

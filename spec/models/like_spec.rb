7e75a97240d9b19a184d67dffcebfee9614ba8c2
ShadyShawkat <shadyshawkat91@gmail.com>
2022-04-01 00:44:58 +0200

    Refactor like_spec test

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
+  user = User.new(
+    name: 'Jim', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+    bio: 'Teacher from Mexico',
+    posts_counter: 0
+  )
+  post = Post.new(
+    author: user, title: 'Hola', text: 'This is my first post',
+    likes_counter: 0, comments_counter: 0
+  )
+  
   subject do
-    Like.new(
-      post: Post.new(
-        author: User.new(
-          name: 'Jim', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-          bio: 'Teacher from Mexico',
-          posts_counter: 0
-        ), title: 'Hola', text: 'This is my first post',
-        likes_counter: 0, comments_counter: 0
-      ),
-      author: User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
-                     posts_counter: 0)
-    )
+    Like.new(post: post, author: user)
   end
 
   before { subject.save }

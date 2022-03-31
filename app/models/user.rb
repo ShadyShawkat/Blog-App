72f6959f3e9eb962c27bb55ae2155c4502fdd075
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-31 22:25:26 +0200

    Validate user posts_counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def last_three_posts
     posts.last(3)

c2caac59fa5bfa5cd72a1dcae2f5186367ad15bc
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-31 20:36:57 +0200

    Add routes for like/create

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
   post 'comments/:post_id', to: 'comments#create'
+  post 'likes/:post_id', to: 'likes#create'
   
   resources :users, only: [:index, :show] do
     resources 'posts', on: :show, only: [:index, :show]

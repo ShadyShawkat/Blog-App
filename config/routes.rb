2213ab6e03d4cbe542c7bd144e22813516aa4e9d
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-31 19:50:07 +0200

    Add routes for Comment/create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'posts/new', to: 'posts#new'
-
+  post 'posts', to: 'posts#create'
+  post 'comments/:post_id', to: 'comments#create'
+  
   resources :users, only: [:index, :show] do
     resources 'posts', on: :show, only: [:index, :show]
   end

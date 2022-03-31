f21d19ef31833c18d0d8f3bd83391d91651a67ed
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-31 19:31:45 +0200

    Add route for Post/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'posts/new', to: 'posts#new'
 
   resources :users, only: [:index, :show] do
     resources 'posts', on: :show, only: [:index, :show]

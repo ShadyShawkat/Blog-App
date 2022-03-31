68d970c3ff7ee062e558b2b9886b270b57baab33
ShadyShawkat <shadyshawkat91@gmail.com>
2022-04-01 00:36:31 +0200

    Fix bug in post_spec tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject { Post.new(user: User.new(name: 'Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0), title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0) }
+  subject { Post.new(author: User.new(name: 'Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0), title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0) }
 
   before { subject.save }
 
@@ -35,6 +35,6 @@ RSpec.describe Post, type: :model do
   end
 
   it 'set_user_posts_counter should return a right result when calling it with after_save' do
-    expect(subject.user.posts_counter).to eq(1)
+    expect(subject.author.posts_counter).to eq(1)
   end
 end

a2a5ff3a4d011c66cc99e41ab2f5e52f6931d732
ShadyShawkat <shadyshawkat91@gmail.com>
2022-04-01 00:16:39 +0200

    Implement eager loading for comments and posts in Post/index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = User.includes(:comments, :posts).find(params[:user_id])
   end
 
   def show

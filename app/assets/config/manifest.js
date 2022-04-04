e9111fb8c889d71e67ca6e628426c5edfdd74707
mre55 <misganawmihreteab>
2022-04-05 00:03:06 +0300

    Register a new user and sign out user

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,4 @@
 //= link_directory ../stylesheets .css
 //= link_directory ../stylesheets/users .css
 //= link_directory ../stylesheets/posts .css
+//= link_directory ../stylesheets/devise/registrations .css

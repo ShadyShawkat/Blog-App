e6e171964c138f94d52065514af769e7b0461ba6
mre55 <misganawmihreteab>
2022-04-08 00:13:57 +0300

    Create an API endpoint to add a comment to a post

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'posts/new', to: 'posts#new'
+  # get 'posts/new', to: 'posts#new'
   # post 'posts', to: 'posts#create'
-  post 'comments/:post_id', to: 'comments#create'
-  post 'likes/:post_id', to: 'likes#create'
-  resources :users, only: [:index, :show] do
+  # post 'comments/:post_id', to: 'comments#create'
+  # post 'likes/:post_id', to: 'likes#create'
+  resources :users, only: [:index, :show, :create] do
     resources :posts, on: :show, only: [:index, :show, :destroy, :create] do
-      resources :comments, only: [:destroy]
+      resources :comments, on: :show, only: [:index, :create, :destroy]
     end
   end
   # Defines the root path route ("/")
